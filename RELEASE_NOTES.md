d53d642388043630e62af68cb3dcb68798fc6361
Dave Carlson <mndrake22@mac.com>
2013-11-23 15:42:45 -0600

    updated FAKE build

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
-#### 0.1.10-beta - Unreleased
+#### 0.1.10-beta - 11/23/2013
 * Added Post Build Task to build documentation based on Excel-DNA .dna file in build folder.
 * Replaces .dna file added by Excel-DNA with one that references ExcelDna.Documentation and has explicit exports for project. 
+
+#### 0.1.11-beta - 11/23/2013
+* deploying using restructured F# MAKE (FAKE) build
--- src/ExcelDnaDoc/Properties/AssemblyInfo.cs
+++ src/ExcelDnaDoc/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("ExcelDnaDoc")]
 [assembly: AssemblyProductAttribute("ExcelDnaDoc")]
 [assembly: AssemblyDescriptionAttribute("command-line utility to create a compiled HTML Help Workshop file (.chm) for ExcelDna")]
-[assembly: AssemblyVersionAttribute("0.1.10")]
-[assembly: AssemblyFileVersionAttribute("0.1.10")]
+[assembly: AssemblyVersionAttribute("0.1.11")]
+[assembly: AssemblyFileVersionAttribute("0.1.11")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.10";
+        internal const string Version = "0.1.11";
     }
 }
