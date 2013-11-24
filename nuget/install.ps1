682a298c460650d03891f04593badf7336bfd111
mndrake <mndrake22@mac.com>
2013-11-24 12:02:16 -0600

    Update install.ps1

--- nuget/install.ps1
+++ nuget/install.ps1
@@ -1,8 +1,8 @@
 param($installPath, $toolsPath, $package, $project)
 # based on Excel-DNA install script
-# Write-Host "Starting ExcelDnaDoc install script"
+Write-Host "Starting ExcelDnaDoc install script"
 
-Write-Host "`tSet reference to ExcelDna.Integration to be CopyLocal=false"
+#Write-Host "`tSet reference to ExcelDna.Integration to be CopyLocal=false"
 #$project.Object.References | Where-Object { $_.Name -eq 'ExcelDna.Integration' } | ForEach-Object { $_.CopyLocal = $false }
 $projName = $project.Name
 $isFSharp = ($project.Type -eq "F#")
