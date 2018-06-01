adbe23c8e9dc470a564fdb3898d6e0032a1605af
Ivan Cronyn <ivan.cronyn@gmail.com>
2018-06-01 12:52:29 +0100

    Adds async support

--- src/ExcelDnaDoc/HtmlHelp.cs
+++ src/ExcelDnaDoc/HtmlHelp.cs
@@ -6,6 +6,7 @@
     using System.IO;
     using System.Linq;
     using System.Reflection;
+    using System.Threading.Tasks;
     using ExcelDna.Documentation.Models;
     using ExcelDnaDoc.Templates;
 
@@ -15,7 +16,7 @@
         public static string HelpContentFolderPath { get; set; }
         public static ConcurrentDictionary<string, string> TemplateCache = new ConcurrentDictionary<string, string>();
 
-        public static void Create(string dnaPath, string helpSubfolder = "HelpContent", bool excludeHidden = false)
+        public static void Create(string dnaPath, string helpSubfolder = "HelpContent", bool excludeHidden = false, bool runAsync = true)
         {
             BuildFolderPath = Path.GetDirectoryName(dnaPath);
             HelpContentFolderPath = Path.Combine(HtmlHelp.BuildFolderPath, helpSubfolder);
@@ -34,26 +35,37 @@
             new TableOfContentsView { Model = addin }.Publish();
             new MethodListView { Model = addin }.Publish();
 
+            //Perhaps better to actually enumerate the values?
+            List<Task> tasks = new List<Task>(runAsync ? 5000 : 0);
+
             foreach (var group in addin.Categories) 
             {
-                new CategoryView { Model = group }.Publish();
+                Action categoryAction = new Action(() => new CategoryView { Model = group }.Publish());
+                Run(categoryAction, tasks, runAsync);
 
                 foreach (FunctionModel function in group.Functions) 
                 {
-                    new FunctionView { Model = function }.Publish();
+                    Action functionAction = new Action(() => new FunctionView { Model = function }.Publish());
+                    Run(functionAction, tasks, runAsync);
                 }
             }
 
             // create Excel Commands content
-
             if (addin.Commands.Count() != 0)
             {
                 new CommandListView { Model = addin }.Publish();
 
                 foreach (var command in addin.Commands)
                 {
-                    new CommandView { Model = command }.Publish();
+                    Action commandAction = new Action(() => new CommandView { Model = command }.Publish());
+                    Run(commandAction, tasks, runAsync);
                 }
+            }
+
+            //Will be empty if async not enabled
+            foreach(Task task in tasks)
+            {
+                task.Wait();
             }
 
             // look for style sheet otherwise use embedded one
@@ -87,6 +99,14 @@
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
 #endif
+        }
+
+        private static void Run(Action action, List<Task> taskList, bool runAsync)
+        {
+            if (runAsync)
+                taskList.Add(Task.Factory.StartNew(action));
+            else
+                action.Invoke();
         }
     }
 }
