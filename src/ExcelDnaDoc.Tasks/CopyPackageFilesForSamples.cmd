fd47810bfda72833e3ecda139eb743ae8cacfb8d
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-03-19 23:41:41 +0500

    Updated CopyPackageFilesForSamples to use only build task files.

--- src/ExcelDnaDoc.Tasks/CopyPackageFilesForSamples.cmd
+++ src/ExcelDnaDoc.Tasks/CopyPackageFilesForSamples.cmd
@@ -1,5 +1,5 @@
 mkdir ..\samples\Package\tools
-copy %1 ..\samples\Package\tools
+xcopy %1* ..\samples\Package\tools /S /I /Y /Q
 
 mkdir ..\samples\Package\build
 copy ExcelDnaDoc.targets ..\samples\Package\build
--- src/ExcelDnaDoc/CopyPackageFilesForSamples.cmd
+++
@@ -1,2 +0,0 @@
-mkdir ..\samples\Package\tools
-xcopy %1* ..\samples\Package\tools /S /I /Y /Q
