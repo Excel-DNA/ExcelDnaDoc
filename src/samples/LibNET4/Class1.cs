80dc41fc7549259fe34d4f19ddacbb79d2f0289d
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-03-20 14:04:08 +0500

    Added ExcelFunctionDoc attribute usage to LibNET4 sample.

--- src/samples/LibNET4/Class1.cs
+++ src/samples/LibNET4/Class1.cs
@@ -1,4 +1,5 @@
-﻿using ExcelDna.Integration;
+﻿using ExcelDna.Documentation;
+using ExcelDna.Integration;
 
 namespace LibNET4
 {
@@ -12,6 +13,19 @@ namespace LibNET4
     [ExcelArgument(Description = "the second string")] object b)
         {
             return string.Concat(a.ToString(), b.ToString());
+        }
+
+        [ExcelFunctionDoc(Name = "Math.AddThem", Category = "Math",
+                    Description = "adds two numbers",
+                    HelpTopic = "DocTest-AddIn.chm!1001",
+                    Summary = "really all it does is add two number ... I promise.",
+                    Example = "Math.AddThem(1, 2) returns 3",
+                    Returns = "the sum of the two arguments")]
+        public static int AddThem(
+[ExcelArgument(Description = "the first argument")] int a,
+[ExcelArgument(Description = "the second argument")] int b)
+        {
+            return a + b;
         }
     }
 }
