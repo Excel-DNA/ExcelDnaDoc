29639ea11343715c2f4891418c8abf22540599e6
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-03-20 19:23:13 +0500

    Added net6.0-windows target and LibNET6 sample project.

--- src/ExcelDnaDoc.Tasks/CopyPackageFilesForSamples.cmd
+++ src/ExcelDnaDoc.Tasks/CopyPackageFilesForSamples.cmd
@@ -1,5 +1,5 @@
 mkdir ..\samples\Package\tools
-xcopy %1* ..\samples\Package\tools /S /I /Y /Q
+xcopy %1..\* ..\samples\Package\tools /S /I /Y /Q
 
 mkdir ..\samples\Package\build
 copy ExcelDnaDoc.targets ..\samples\Package\build
