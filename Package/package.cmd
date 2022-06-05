5fa28ac70b5a2a7f808a141ebd879fa6846361fa
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2022-06-05 15:46:36 +0500

    Added PackageVersion.

--- Package/package.cmd
+++ Package/package.cmd
@@ -4,13 +4,14 @@ setlocal
 set currentPath=%~dp0
 set basePath=%currentPath:~0,-1%
 set outputPath=%basePath%\nupkg
+set ExcelDnaVersion=%1
 
 if exist "%outputPath%\*.nupkg" del "%outputPath%\*.nupkg"
 
 if not exist "%outputPath%" mkdir "%outputPath%"
 
 echo on
-nuget.exe pack "%basePath%\ExcelDnaDoc\ExcelDnaDoc.nuspec" -BasePath "%basePath%\ExcelDnaDoc" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive
+nuget.exe pack "%basePath%\ExcelDnaDoc\ExcelDnaDoc.nuspec" -BasePath "%basePath%\ExcelDnaDoc" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%"
 @if errorlevel 1 goto end
 
 :end
