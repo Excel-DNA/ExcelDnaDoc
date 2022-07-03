24fa76a23e9ccbe7e2bf805e192f434aacb1dadd
Govert van Drimmelen <govert@dnakode.com>
2022-07-04 01:43:18 +0200

    Update NuGet package spec

---
+++ Package/push.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+set currentPath=%~dp0
+set basePath=%currentPath:~0,-1%
+set outputPath=%basePath%\nupkg
+
+nuget.exe push "%outputPath%\ExcelDnaDoc.1.6.0-preview4.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+@if errorlevel 1 goto end
